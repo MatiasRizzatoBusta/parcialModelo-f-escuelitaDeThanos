46d792ad025de50f895ace0fb5ee39dcff1ed089
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-09 16:48:15 -0300

    pto 4

--- src/Lib.hs
+++ src/Lib.hs
@@ -25,11 +25,11 @@ data Universo = UnUniverso{
 }deriving (Show,Eq)
 
 chasquido :: Guante->Universo->Universo
-chasquido guante universo |((== 6).length.gemas) guante = universo{habitantes= reduzco (habitantes universo)}
+chasquido guante universo |((== 6).length.gemas) guante = universo{habitantes= reduzcoUniverso (habitantes universo)}
                           |otherwise = universo
 
-reduzco :: [Personaje]->[Personaje]
-reduzco habitantes = take (laMitad habitantes) habitantes
+reduzcoUniverso :: [Personaje]->[Personaje]
+reduzcoUniverso habitantes = take (laMitad habitantes) habitantes
 
 laMitad :: [Personaje]->Int
 laMitad habitantes = (length habitantes) `div `2
@@ -47,4 +47,37 @@ energiaUniverso :: Universo->Int
 energiaUniverso  = sum.map energia.losQueTienenMasDeUnaHabilidad.habitantes
 
 losQueTienenMasDeUnaHabilidad :: [Personaje]->[Personaje]
-losQueTienenMasDeUnaHabilidad  =  filter ((>1).length.habilidades) --agarro un pj,voy a sus habilidades les hago length y
+losQueTienenMasDeUnaHabilidad  =  filter ((>1).length.habilidades) --agarro un pj,voy a sus habilidades les hago length y comparo
+
+------------------------------------- Punto 3 -------------------------------------
+laMente :: Int->Gema
+laMente debilitar personaje = personaje{energia= (energia personaje) - debilitar}
+
+elAlma :: Habilidad->Gema
+elAlma habilidadASacar personaje = personaje{habilidades= sacoHabilidad habilidadASacar (habilidades personaje)}
+
+sacoHabilidad :: Habilidad->[String]->[String]
+sacoHabilidad habilidadASacar = filter (not.(== habilidadASacar))
+
+elEspacio :: Gema
+elEspacio personaje = personaje{energia = (energia personaje) - 20,planeta = "Endor"}
+
+elPoder :: Gema
+elPoder personaje = personaje{energia=0, habilidades = dejoSinHabilidades (habilidades personaje)}
+
+dejoSinHabilidades :: [Habilidad]->[Habilidad]
+dejoSinHabilidades listaHabilidades |((<=2).length) listaHabilidades =  [] --si tiene 2 o menos devuelvo lista vacia
+                                    |otherwise = listaHabilidades--si tiene mas no lo cambio
+elTiempo :: Gema
+elTiempo personaje = personaje{edad = reducirEdad (edad personaje)}
+
+reducirEdad :: Int->Int
+reducirEdad edad = max 18 (edad `div` 2)
+
+laGemaLoca :: Gema->Gema --la gema que quiero usar se la paso como parametro
+laGemaLoca gema = gema.gema
+
+------------------------------------- Punto 4 -------------------------------------
+guanteDeGoma = UnGuante "goma" [elTiempo,elAlma "usar Mjolir",laGemaLoca (elAlma "programacion en haskell")]
+
+------------------------------------- Punto 5 -------------------------------------
